9a58ffc62968b5789d6ecb74ff3317c6b0567684
shubham-mhatre <mhatreshubham03@gmail.com>
2025-02-19 19:22:31 +0530

    project setup

---
+++ src/main/java/com/sm/azblb/SpringbootAzureBlobStorageApplication.java
@@ -0,0 +1,13 @@
+package com.sm.azblb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAzureBlobStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAzureBlobStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sm/azblb/SpringbootAzureBlobStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.azblb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAzureBlobStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
